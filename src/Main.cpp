d6cf60340c989b7b656bd59d21bd91436425592f
wuzewei <wuzewei@didiglobal.com>
2019-10-19 17:22:10 +0800

    1

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 int main() {
-    CardTable card_table;
-    card_table.add_card("Ad");
     card_table.add_card("2d");
     card_table.add_card("3d");
     card_table.add_card("4d");
     card_table.add_card("5d");
-    card_table.add_card("Tc");
-    card_table.add_card("Jc");
+    card_table.add_card("Td");
+    card_table.add_card("Jd");
+    card_table.add_card("Qd");
+    card_table.add_card("Kd");
+    card_table.add_card("Ad");
     cout << card_table.isStraightFlush() << endl;
 }
